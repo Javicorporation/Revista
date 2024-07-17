1386e9b533df0fb1a810782ac711589cb7815a85
Javicorporation <krchipullajavier@gmail.com>
2024-07-17 00:35:32 -0500

    implementacion de servicios y componentes

---
+++ src/app/services/firestore.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { BehaviorSubject } from 'rxjs';
+import { Articulo } from '../modelsDatabase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+  private articuloSource = new BehaviorSubject<Articulo | null>(null);
+  currentArticulo = this.articuloSource.asObservable();
+
+  constructor(public database: AngularFirestore) { }
+  //funcion crear articulo
+  crearArticulo(data: any, path: string, id: string){
+    const coleccion = this.database.collection(path);
+    return coleccion.doc(id).set(data);
+  }
+
+  //funcion obtener articulo
+  obtenerArticulo(path: string, id: string){
+    const coleccion = this.database.collection(path);
+    return coleccion.doc(id).valueChanges();
+  }
+
+  //funcion para eliminar un articulo
+  eliminarArticulo(path: string, id: string){
+    const coleccion = this.database.collection(path);
+    return coleccion.doc(id).delete();
+  }
+
+  // funcion para actualizar un articulo
+  actulizarArticulo(data: string, path: string, id: string){
+    const coleccion = this.database.collection(path);
+    return coleccion.doc(id).update(data);
+  }
+
+  getId(){
+    return this.database.createId();
+  }
+
+
+  obtenerLaColeccionXD(path: string){
+    const coleccion = this.database.collection(path);
+    return coleccion.valueChanges();
+  }
+
+}
+  obtenerLaColeccionXD<Tipo>(path: string){
+    const coleccion = this.database.collection<Tipo>(path);
+    return coleccion.valueChanges();
+  }
+
+  changeArticulo(articulo: Articulo) {
+    this.articuloSource.next(articulo);
+  }
+}
+
